eee7ce1529f58aacdb5f63c4d2e5cd1026850dbb
Neon Engfer <nengfer@gmail.com>
2014-04-13 11:36:42 -0700

    Implement removal of completed todos.  Add some css

--- js/todo.js
+++ js/todo.js
@@ -5,7 +5,7 @@
  * for actual development we would properly modularize our
  * code.
  */
-function TodoCtrl($scope) {
+function TodoCtrl($scope, $filter) {
   // Define our todo container
   $scope.todoList = [];
   
@@ -21,5 +21,11 @@ function TodoCtrl($scope) {
     
     // Reset our input text so we can easily add more entries
     $scope.todo.text = '';
+  }
+  
+  // Define how to remove completed items from our list.
+  $scope.deleteCompleted = function() {
+    // Use our $filter helper to limit our list to only those that are not completed.
+    $scope.todoList = $filter('filter')($scope.todoList, {isCompleted: false});    
   }
 }
